eb7961944cac3a402350a36e870fc6e6b14edda5
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-16 13:35:42 -0300

    valor promocional

--- src/ex013.java
+++ src/ex013.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class ex013 {
+public class ex013{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o preço do produto: R$");
+        double precoProduto = Double.parseDouble(input.nextLine());
+
+        double precoPromocional = precoProduto - ((precoProduto * 5) / 100);
+        System.out.printf("Valor promocional do produto R$%.2f", precoPromocional);
+    }
 }
