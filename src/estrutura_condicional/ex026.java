caf1c21c3e11ffa5ae0be60f6c18cc662e4b0d97
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-18 16:02:34 -0300

    formando triangulos

--- src/estrutura_condicional/ex026.java
+++ src/estrutura_condicional/ex026.java
@@ -1,4 +1,32 @@
-package 2_estrutura_sequencial;
+package estrutura_condicional;
+import java.util.Scanner;
 
 public class ex026 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o comprimento do primeiro segmento: ");
+        double ladoA = Double.parseDouble(input.nextLine());
+        System.out.print("Digite o comprimento do segundo segmento: ");
+        double ladoB = Double.parseDouble(input.nextLine());
+        System.out.print("Digite o comprimento do terceiro segmento: ");
+        double ladoC = Double.parseDouble(input.nextLine());
+
+        /*
+        Matematicamente, para três segmentos formarem um triângulo,
+        o comprimento de cada lado deve ser menor que a soma dos outros dois.
+        */
+        boolean formaTriangulo = (
+            ladoA < ladoB + ladoC &&
+            ladoB < ladoA + ladoC &&
+            ladoC < ladoB + ladoA
+        );
+
+        if(formaTriangulo){
+            System.out.print("Os três segmentos formam um triângulo!");
+        } else {
+            System.out.print("Os três segmentos NÃO formam um triângulo!");
+        }
+
+    }
 }
