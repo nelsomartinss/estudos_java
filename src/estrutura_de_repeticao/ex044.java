ce763829a330c5016bd7e7a2c4617e742d172ea9
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-21 13:33:35 -0300

    contagem de 6 a 11

---
+++ src/estrutura_de_repeticao/ex044.java
@@ -0,0 +1,12 @@
+package estrutura_de_repeticao;
+
+public class ex044 {
+    public static void main(String[] args){
+        int i = 6;
+        while(i <= 11){
+            System.out.print(i + " ");
+            i+=1;
+        }
+        System.out.print("Acabou!");
+    }
+}
