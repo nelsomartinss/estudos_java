f777d46dbf280f47efaacf712f3dd0d4c8578da4
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-21 13:36:41 -0300

    diminuindo de 10 a 3

---
+++ src/estrutura_de_repeticao/ex045.java
@@ -0,0 +1,12 @@
+package estrutura_de_repeticao;
+
+public class ex045 {
+    public static void main(String[] args){
+        int i = 10;
+        while(i >= 3){
+            System.out.print(i + " ");
+            i-=1;
+        }
+        System.out.print("Acabou!");
+    }
+}
