ddf7aa01575a64f3621d297f15b7a8d6c849291b
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-21 13:39:04 -0300

    loop somando de 3 em 3

---
+++ src/estrutura_de_repeticao/ex046.java
@@ -0,0 +1,12 @@
+package estrutura_de_repeticao;
+
+public class ex046 {
+    public static void main(String[] args){
+        int i = 0;
+        while(i <= 18){
+            System.out.print(i + " ");
+            i+=3;
+        }
+        System.out.print("Acabou!");
+    }
+}
