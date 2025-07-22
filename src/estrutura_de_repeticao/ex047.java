7d4cc76a8ed3df7787fd846302a9ea0704b35eff
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-22 15:26:59 -0300

    decrementando de 100 a 0

---
+++ src/estrutura_de_repeticao/ex047.java
@@ -0,0 +1,12 @@
+package estrutura_de_repeticao;
+
+public class ex047 {
+    public static void main(String[] args){
+        int i = 100;
+        while(i >= 0){
+            System.out.print(i + " ");
+            i-=5;
+        }
+        System.out.print("Acabou!");
+    }
+}
