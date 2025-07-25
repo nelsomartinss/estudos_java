b8a5264e27358d1dfa0a53c613fa7a2da6357b3e
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-25 13:32:07 -0300

    incrementando com do while

---
+++ src/estrutura_de_repeticao/ex067.java
@@ -0,0 +1,12 @@
+package estrutura_de_repeticao;
+
+public class ex067 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.print(i + " ");
+            i += 3;
+        } while (i <= 30);
+        System.out.print("Acabou!");
+    }
+}
