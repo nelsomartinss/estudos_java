0bebdd322b364d1f2aff334d296d754614ac9464
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-25 19:26:02 -0300

    decrementando com loop for

---
+++ src/estrutura_de_repeticao/ex071.java
@@ -0,0 +1,10 @@
+package estrutura_de_repeticao;
+
+public class ex071 {
+    public static void main(String[] args){
+        for(int i = 100; i >= 0; i-=10){
+            System.out.print(i + " ");
+        }
+        System.out.print("Acabou!");
+    }
+}
