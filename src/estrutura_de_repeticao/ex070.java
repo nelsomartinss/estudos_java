2964630e5e28d2a2d7cc70ec0ef0cb7307fa2c8f
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-25 19:24:25 -0300

    contagem do loop for

---
+++ src/estrutura_de_repeticao/ex070.java
@@ -0,0 +1,10 @@
+package estrutura_de_repeticao;
+
+public class ex070 {
+    public static void main(String[] args){
+        for(int i = 0; i <= 40; i+=5){
+            System.out.print(i + " ");
+        }
+        System.out.print("Acabou!");
+    }
+}
