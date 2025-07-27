52fa14c2538f822b58563b5a8d644b7ed1a7b8e0
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-26 21:43:12 -0300

    vetor númerico com os 15 primeiros numeros da sequencia fibonacci

--- src/estrutura_de_dados/ex080.java
+++ src/estrutura_de_dados/ex080.java
@@ -8,15 +8,17 @@ public class ex080 {
         int segundoNumero = 1;
         int terceiroNumero;
 
+        // primeiro é necessário adicionar os 2 primeiros numeros a array
         vetorNumerico.add(primeiroNumero);
         vetorNumerico.add(segundoNumero);
 
         for(int i = 0; i < 15; i++){
-            terceiroNumero = primeiroNumero + segundoNumero;
-            vetorNumerico.add(terceiroNumero);
+            terceiroNumero = primeiroNumero + segundoNumero; // depois somamos os dois primeiros para termos o terceiro
+            vetorNumerico.add(terceiroNumero); // agora é só adicionar o terceiro
 
-            primeiroNumero = segundoNumero;
-            segundoNumero = terceiroNumero;
+            primeiroNumero = segundoNumero; // depois o primeiro vira o segundo
+            segundoNumero = terceiroNumero; // e o segundo vira o terceiro
         }
+        System.out.println(vetorNumerico);
     }
 }
