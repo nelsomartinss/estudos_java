2a1c0f1520e293ae7e2fdc212a65b733f48c504f
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-28 19:23:22 -0300

    Lista de números e lista de números múltiplos de 10

---
+++ src/estrutura_de_dados/ex083.java
@@ -0,0 +1,25 @@
+package estrutura_de_dados;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ex083 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+        ArrayList<Integer> listaMultiplosDe10 = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            System.out.print("Digite um número: ");
+            int entradaNumeros = Integer.parseInt(input.nextLine());
+            listaNumeros.add(entradaNumeros);
+
+            if(entradaNumeros % 10 == 0){
+                listaMultiplosDe10.add(entradaNumeros);
+            }
+        }
+        System.out.printf("Lista de números: %s\n", listaNumeros);
+        System.out.printf("Números múltiplos de 10: %s\n", listaMultiplosDe10);
+
+
+    }
+}
