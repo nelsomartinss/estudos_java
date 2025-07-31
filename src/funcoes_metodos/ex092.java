796d0424f9f4f91096a0e7ce87f8ad705db11097
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-31 20:23:26 -0300

    Procedimento de exibe uma mensagem

---
+++ src/funcoes_metodos/ex092.java
@@ -0,0 +1,17 @@
+package funcoes_metodos;
+
+public class ex092 {
+
+    // criando procedimento
+    public static void Gerador() {
+        System.out.println("""
+                +-------=======------+
+                      Olá, Mundo!
+                +-------=======------+
+                """);
+    }
+
+    public static void main(String[] args){
+        Gerador(); // chamando procedimento
+    }
+}
