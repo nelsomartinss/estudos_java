250ab06f23e3f762da03c74fdadc03d9a95cfa0c
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-08-01 15:44:14 -0300

    procedimento que verifica se numero é par ou impar

--- src/funcoes_metodos/ex098.java
+++ src/funcoes_metodos/ex098.java
@@ -1,4 +1,21 @@
 package funcoes_metodos;
+import java.util.Scanner;
 
 public class ex098 {
+    public static void parOuImpar(int numero){
+        if(numero % 2 == 0){
+            System.out.printf("Número %d é PAR!", numero);
+        } else {
+            System.out.printf("Número %d é IMPAR!", numero);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int entradaNumero = Integer.parseInt(input.nextLine());
+
+        parOuImpar(entradaNumero);
+    }
 }
